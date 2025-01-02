16e39e5f684202043507d005d4df3fb64d602cad
hao0609 <cnblue603@gmail.com>
2025-01-02 20:04:26 +0800

    add

---
+++ vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+
+</script>
+
+
+<style>
+    @font-face {
+        font-family: 'Dela Gothic One';
+        src: url('./assets/fonts/DelaGothicOne-Regular.ttf') format('truetype');
+
+        font-family: 'NotoSansTC';
+        src: url('./assets/fonts/NotoSansTC-VariableFont_wght.ttf') format('truetype');
+    }
+
+
+    @media screen {
+        
+    }
+</style>
+
+<template>
+    <Navbar/>
+    <div class="wrapper">
+    </div>
+
+</template>
