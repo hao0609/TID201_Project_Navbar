7d972b6a4a88655763bc8e93142c345f87922006
hao0609 <cnblue603@gmail.com>
2025-01-07 17:47:48 +0800

    add

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,21 +5,6 @@ import Navbar from './components/Navbar.vue'
 </script>
 
 
-<style>
-    @font-face {
-        font-family: 'Dela Gothic One';
-        src: url('./assets/fonts/DelaGothicOne-Regular.ttf') format('truetype');
-
-        font-family: 'NotoSansTC';
-        src: url('./assets/fonts/NotoSansTC-VariableFont_wght.ttf') format('truetype');
-    }
-
-
-    @media screen {
-        
-    }
-</style>
-
 <template>
     <Navbar/>
     <div class="wrapper">
