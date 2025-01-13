d72eaf9d89153ed7296a57754ad931cbdfe973ee
hao0609 <cnblue603@gmail.com>
2025-01-13 12:56:19 +0800

    update 0113

--- vue-project/src/components/Navbar.vue
+++ vue-project/src/components/Navbar.vue
@@ -3,7 +3,27 @@
 import icon from './icons/icon.vue' 
 import shopping_cart from './icons/shopping_cart.vue' 
 import user from './icons/user.vue' 
-import hamburger from './icons/hamburger.vue'
+// import hamburger from './icons/hamburger.vue'
+
+import { ref } from 'vue';
+
+const checked = ref(false);
+const rwd_menu = ref(null);
+
+const checked_change = () => {
+
+    
+    if (checked.value === true && rwd_menu.value) {
+
+        rwd_menu.value.style.visibility = 'visible';
+        rwd_menu.value.style.transform = 'scale(1)';
+
+    } else if (checked.value === false ) {
+        console.log("test");
+        rwd_menu.value.style.visibility = 'hidden';
+        rwd_menu.value.style.transform = 'scale(0)';
+    }
+};
 
 </script>
 
@@ -33,19 +53,20 @@ import hamburger from './icons/hamburger.vue'
         </nav>
 
 
-        <hamburger/>
-
-        <!-- <div class="hamburger_box">
-            <input type="checkbox" class="hamburger_check" @change="checked">
+        <div class="hamburger_box" >
+            <input type="checkbox" class="hamburger_check"  @change="checked_change" v-model="checked">
             <div class="hamburger">
-                <div></div>
-            </div>
-        </div> -->
+                <div>
+                    
+                </div>
+        </div>
+
+</div>
 
 
     </header>
 
-    <div class="rwd_menu">
+    <div class="rwd_menu" ref="rwd_menu">
             <div class="rwd_menu_content">
                 <div class="tittle_box">
             
@@ -68,70 +89,89 @@ import hamburger from './icons/hamburger.vue'
 </template>
 
 <style>
-
- .icon_text{
-    font-family: "Dela Gothic One";
-    font-size: 24px;
-    color: white;
- }
- .icon{
-    display: flex;
-    gap: 15px;
-    align-items: center;
- }
- header{
-    display: flex;
+.rwd_menu{
+    background-color: #e0c4ee;
+    position: absolute;
     width: 100%;
-    background-color: black;
-    height: 72px;
-    /* position: absolute; */
-    padding: 0px 40px;
-    justify-content:space-between
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    visibility: hidden;
+    transition: all .4s ease;
+    border-radius: 10%;
+    transform: scale(0);
+}
+.rwd_menu_content{
+    width: 100%;
+    display: flex;
 
- }
- ul{
-    list-style: none;
-    display: flex;
- }
- nav{
-    display: flex;
-    align-items: center;
- }
-.menu{
-    font-size: 16px;
-    font-family: 'NotoSansTC';
-    display: flex;
-    padding: 0;
-    gap: 32px;
-    color: white;
- }
- .line{
-    width: 1px;
-    height: 21px;
-    background: white;
-    margin: 0 36px;
- }
- .shopping_cart{
-    margin-right: 36px;
- }
- .hamburger_box{
-    display: flex;
-    align-items: center;
-    display: none;
-    cursor: pointer;
- }
- @media screen and (max-width: 800px) {
-   
-   nav{
-      display: none;
-   }
-   .hamburger_box{
-      display: flex;
-   }
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin-top: 112px;
+    padding: 0 30px;
+}
 
- }
+.tittle_box{
+    height: 336px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    color: var(--Primary-Primary-950, #460961);
+    text-align: left;
+    font-feature-settings: 'liga' off, 'clig' off;
 
- 
+    /* [Mobile]/H2 Regular(m) */
+    font-family: "Dela Gothic One";
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 52px; 
+}
+
+.icon_box{
+    display: flex;
+    padding: 5px 0px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 12px;
+    border-radius: 20px;
+    background: var(--primary-primary-400, #8C25C0);
+
+}
+.bg_image{
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+}
+.blue_man{
+    display: block;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    z-index: 2;
+}
+.yellow_man {
+    display: block;
+    position: absolute;
+    right: 150px;
+    bottom: 5px;
+    z-index: 1;
+}
+
+.blue_man>img{
+    display: block;
+    width: 100%;
+
+}
+.yellow_man>img{
+    display: block;
+    width: 100%;
+
+}
 
 </style>
 
--- vue-project/src/components/icons/hamburger.vue
+++ vue-project/src/components/icons/hamburger.vue
@@ -1,13 +1,5 @@
 <template>
-<div class="hamburger_box">
-    <input type="checkbox" class="hamburger_check"  @change="$emit('change', $event)">
-    <div class="hamburger">
-        <div>
-            
-        </div>
-    </div>
 
-</div>
 </template>
 
 
