954d127bc1963d06418d7d9135ba5fa8a3424543
hao0609 <cnblue603@gmail.com>
2025-02-17 02:25:04 +0800

    剩文湖線沒畫完

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -6,6 +6,11 @@ import Navbar_V3 from '../components/Navbar_V3.vue'
 import login from '../view/login.vue'
 import alert_test from '../view/alert_test.vue'
 import MissionGeneralView from '../view/MissionGeneralView.vue'
+import red_line from '../view/line/red.vue'
+import green_line from '../view/line/green.vue'
+import blue_line from '../view/line/blue.vue'
+import yellow_line from '../view/line/yellow.vue'
+import brown_line from '../view/line/brown.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -19,7 +24,38 @@ const router = createRouter({
     {
       path: '/',
       name: 'MissionGeneralView',
-      component: MissionGeneralView
+      component: MissionGeneralView,
+      redirect: '/red', // 讓 `/` 預設導向 `/red
+      children: [
+        {
+          path: 'red', // 這裡是子路由，不要加 `/`
+          name: 'Red',
+          component: red_line,
+        },
+        {
+          path: 'green', // 這裡是子路由，不要加 `/`
+          name: 'Green',
+          component: green_line,
+        },
+        {
+          path: 'blue', // 這裡是子路由，不要加 `/`
+          name: 'Blue',
+          component: blue_line,
+        },
+        {
+          path: 'yellow', // 這裡是子路由，不要加 `/`
+          name: 'Yellow',
+          component: yellow_line,
+        },
+        {
+          path: 'brown', // 這裡是子路由，不要加 `/`
+          name: 'Brown',
+          component: brown_line,
+        },
+
+        
+
+      ]
     },
    
     {
---
+++ vue-project/src/view/line/yellow.vue
@@ -0,0 +1,47 @@
+<script setup>
+    import { ref } from 'vue'
+    import ground from '../../assets/images/MessionGeneral/ground.png';
+    import building_tree from '../../assets/images/MessionGeneral/building_tree.png'
+    import road from '../../assets/images/MessionGeneral/road.png';
+    import bridge from '../../assets/images/MessionGeneral/bridge.png';
+    import station_yellow from '../../assets/images/MessionGeneral/station_yellow.vue';
+    import logo from '../../assets/images/MessionGeneral/logo.vue';
+    import pin from '../../assets/images/MessionGeneral/pin.vue';
+    import { pinjs } from '../../js/view/MissionGeralView/pin.js'             // 引入 pin.js
+    const { pinStyle } = pinjs();                             // 使用 pin.js 的 pin()
+    import alert_user_location from '@/alert/alert_user_location.vue';
+
+    const alert_web_M_userlocation = ref(null);
+    const UserLocationShowAlert = () => {
+    alert_web_M_userlocation.value.UserLocationShowAlert();  
+    }
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="item">
+            <img :src=ground class="bg_image" >
+            
+        </div>
+        <div class="item">
+            <img :src=road class="bg_image" >
+        </div>
+        <div class="item">
+            <img :src=bridge class="bg_image" >
+        </div>
+        <div class="item">
+            <station_yellow class="station"/>
+        </div>
+        <div class="item">
+            <img :src=building_tree class="bg_image" >
+        </div>
+        <div class="item">
+            <logo class="logo"/>
+        </div>
+    </div>
+    <div class="hitarea">
+        <!-- <button ref="pin_obj" class="pin" :style="pinStyle" @click="UserLocationShowAlert" @touchstart="UserLocationShowAlert" ><pin/></button> -->
+        <!-- <pin/> -->
+    </div>
+    <alert_user_location ref="alert_web_M_userlocation"/> 
+</template>
